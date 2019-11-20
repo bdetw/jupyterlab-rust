7773cb33df21cd2d9adc712964fd429de5806b6e
Alfred456654 <augiraud@gmail.com>
2019-11-21 00:23:31 +0100

    tag builds

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t jupyterlab-rust:latest .
+NOW="$(date +%Y%m%d_%H%M%S)"
+docker build -t alfred456654/jupyterlab-rust:$NOW .
+docker tag alfred456654/jupyterlab-rust:$NOW alfred456654/jupyterlab-rust:latest
